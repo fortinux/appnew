62e2528c91d59c841c1dda79cae8d9b94e0daa29
fortinux <info@fortinux.com>
2022-10-03 20:05:46 +0200

    added README

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Author: Fortinux
 info@fortinux.com
 Date: [Add date here]
 """
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -14,19 +14,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<H1>Esta es la página principal</H1>"
+    return render_template("index.html")
 
 
 @app.route("/servicios")
 def servicios():
-    return "<H1>Esta es la página de servicios</H1>"
+    return render_template("base.html")
 
 
 @app.route("/contacto")
 def contacto():
-    return "<H1>Esta es la página de contacto</H1>"
+    return render_template("base.html")
 
 
 @app.route("/admin")
 def admin():
-    return "<H1>Esta es la página de admin</H1>"
+    return render_template("base.html")
