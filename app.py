859ae96ef722cdad8cc63683c27da69a0be587a1
fortinux <info@fortinux.com>
2022-10-03 19:59:03 +0200

    html commit

--- app.py
+++ app.py
@@ -8,9 +8,25 @@ Date: [Add date here]
 """
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return "<H1>Hola Mundo!</H1>"
+def home():
+    return "<H1>Esta es la página principal</H1>"
+
+
+@app.route("/servicios")
+def servicios():
+    return "<H1>Esta es la página de servicios</H1>"
+
+
+@app.route("/contacto")
+def contacto():
+    return "<H1>Esta es la página de contacto</H1>"
+
+
+@app.route("/admin")
+def admin():
+    return "<H1>Esta es la página de admin</H1>"
